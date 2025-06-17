9931eb886a907b71f1e34c96a82e96194a4b3c76
Patrick Mast <patrickmast@mac.com>
2025-06-17 08:57:21 -0700

    refactor: Remove deprecated color and token definitions from style guide

--- colors.d.ts
+++
@@ -1,6 +0,0 @@
-export declare const colors: {
-    primary: string;
-    border: string;
-    inputBg: string;
-    foreground: string;
-};
--- tokens.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const tokens: {
-    borderRadius: string;
-};
